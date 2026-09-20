39cb47345fb8c5d9ac58390a6145eb09fc3bdb14
DariaPoliakovaa <dashacovic@gmail.com>
2026-09-20 22:04:19 +0300

    Update Storage.ts

--- src/services/Storage.ts
+++ src/services/Storage.ts
@@ -6,15 +6,14 @@ export class Storage {
     public static get<T>(key: string): T | null {
         const data = localStorage.getItem(key);
         if (!data) return null;
-        
-                try {
+
+        try {
             return JSON.parse(data) as T;
         } catch (error) {
             // eslint-disable-next-line no-console
             console.error(`Помилка парсингу даних для ключа ${key}:`, error);
             return null;
         }
-
     }
 
     public static remove(key: string): void {
